c79d82b64071e8a94bb186ece36bd8f5ed2ca6df
pandadev666 <smartguy.panda666@gmail.com>
2018-10-13 15:11:26 -0700

    Update unit_tests with Travis-CI

--- pytholenium/unit_tests.py
+++ pytholenium/unit_tests.py
@@ -4,11 +4,18 @@ from selenium import webdriver
 import os
 
 class TestPytholenium(unittest.TestCase):
-    
-    webdriver_path = os.path.dirname(os.path.realpath(__file__)) + r'/test_data/chromedriver_v2.42.exe'
+    webdriver_path = os.path.dirname(os.path.realpath(__file__)) + r'/test_data/chromedriver'
     webpage_path = os.path.dirname(os.path.realpath(__file__)) + r'/test_data/test.html'
-    driver = webdriver.Chrome(webdriver_path)
+
+    options = webdriver.ChromeOptions()
+    options.binary_location = '/usr/bin/chromium-browser'
+    options.add_argument("--no-sandbox")
+    options.add_argument("--no-default-browser-check")
+    options.add_argument("--no-first-run")
+    options.add_argument("--disable-default-apps")
+    driver = webdriver.Chrome(webdriver_path, chrome_options=options)
     driver.get(webpage_path)
+
 
     #TC1 - Get element by id
     def test_TC1 (self, driver=driver):
@@ -18,6 +25,7 @@ class TestPytholenium(unittest.TestCase):
         #Validate
         self.assertEqual(text, "Hey there!")
 
+
     #TC2 - Get element by name
     def test_TC2 (self, driver=driver):
         #Do the steps
@@ -26,15 +34,6 @@ class TestPytholenium(unittest.TestCase):
         #Validate
         self.assertEqual(text, "I'm Pytholenium test.html")
 
-
-
-
-    #TCX - Close driver
-    def test_TCX (self, driver=driver):
-        #Do the steps
-        driver.quit()
-        #Validate
-        self.assertTrue(True)
 
 
 
